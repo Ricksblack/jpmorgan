dd1a63ddeebb37ee6e56013590d389fc7b9ffa43
Ricardo Moreno <rmm_92@outlook.com>
2023-04-15 11:06:19 -0700

    moved WeatherViewModel to its own file

--- Coordinator/Feature/WeatherView.swift
+++ Coordinator/Feature/WeatherView.swift
@@ -7,11 +7,6 @@
 
 import Foundation
 
-struct WeatherViewModel: Equatable {
-    let name: String
-    let temperature: String
-}
-
 enum WeatherViewState: Equatable {
     case idle
     case updateWeather(viewModel: WeatherViewModel)
---
+++ Coordinator/Feature/WeatherViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherViewModel.swift
+//  Coordinator
+//
+//  Created by Ricardo Moreno on 4/15/23.
+//
+
+import Foundation
+
+struct WeatherViewModel: Equatable {
+    let name: String
+    let temperature: String
+}
