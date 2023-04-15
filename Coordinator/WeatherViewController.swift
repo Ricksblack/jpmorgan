6c4b0bfdcce715682f11c1a71d2c2c6ba62e4243
Ricardo Moreno <rmm_92@outlook.com>
2023-04-15 14:29:52 -0700

    using loader for image icon

--- Coordinator/WeatherViewController.swift
+++ Coordinator/WeatherViewController.swift
@@ -48,6 +48,7 @@ private extension WeatherViewController {
 
     func handleUpdateWeather(_ viewModel: WeatherViewModel) {
         cityNameLabel.text = viewModel.cityName
+        iconImage.loadImageUsingCache(withUrl: viewModel.iconURL)
         degreesLabel.text = viewModel.currentDegrees
         weatherDescription.text = viewModel.weatherDescription
         highestTemperature.text = viewModel.highestDegrees
@@ -83,43 +84,5 @@ extension WeatherViewController: WeatherViewContract {
         case .errorLoadingWeather(let city):
             handleErrorLoadingWeather(for: city)
         }
-    }
-}
-
-
-let imageCache = NSCache<NSString, UIImage>()
-extension UIImageView {
-    func loadImageUsingCache(withUrl urlString : String) {
-        let url = URL(string: urlString)
-        if url == nil {return}
-        self.image = nil
-
-        // check cached image
-        if let cachedImage = imageCache.object(forKey: urlString as NSString)  {
-            self.image = cachedImage
-            return
-        }
-
-        let activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView.init(style: .medium)
-        addSubview(activityIndicator)
-        activityIndicator.startAnimating()
-        activityIndicator.center = self.center
-
-        // if not, download image from url
-        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
-            if error != nil {
-                print(error!)
-                return
-            }
-
-            DispatchQueue.main.async {
-                if let image = UIImage(data: data!) {
-                    imageCache.setObject(image, forKey: urlString as NSString)
-                    self.image = image
-                    activityIndicator.removeFromSuperview()
-                }
-            }
-
-        }).resume()
     }
 }
