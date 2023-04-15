7039b5ad5def1695e4648bfaf7df8e82aad78b7c
Ricardo Moreno <rmm_92@outlook.com>
2023-04-15 11:19:03 -0700

    Created file for GetWeatherUseCase

---
+++ Coordinator/Domain/GetWeatherUseCase.swift
@@ -0,0 +1,12 @@
+//
+//  GetWeatherUseCase.swift
+//  Coordinator
+//
+//  Created by Ricardo Moreno on 4/15/23.
+//
+
+import Foundation
+
+protocol GetWeatherUseCase {
+    func run(city: String)
+}
