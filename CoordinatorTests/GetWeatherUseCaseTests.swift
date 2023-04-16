50df01629dbca4544423d72756dc722bec14a623
Ricardo Moreno <rmm_92@outlook.com>
2023-04-16 14:19:30 -0700

    added GetWeatherUseCaseTests

---
+++ CoordinatorTests/GetWeatherUseCaseTests.swift
@@ -0,0 +1,12 @@
+//
+//  GetWeatherUseCaseTests.swift
+//  CoordinatorTests
+//
+//  Created by Ricardo Moreno on 4/16/23.
+//
+
+import XCTest
+
+final class GetWeatherUseCaseTests: XCTestCase {
+
+}
