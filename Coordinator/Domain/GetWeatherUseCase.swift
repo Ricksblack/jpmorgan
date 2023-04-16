03753232313e69cf2df6e77f572e13de4aaf974a
Ricardo Moreno <rmm_92@outlook.com>
2023-04-16 12:03:28 -0700

    added fromLocationIfAvailable for GetWeatherUseCase

--- Coordinator/Domain/GetWeatherUseCase.swift
+++ Coordinator/Domain/GetWeatherUseCase.swift
@@ -9,9 +9,9 @@ import Foundation
 
 protocol GetWeatherUseCase {
     typealias WeatherByCityUseCaseCompletion = (Result<WeatherModel, Error>) -> Void
-    typealias WeatherByLocationUseCaseCompletion = (Result<WeatherModel, Error>) -> Void
 
     func run(city: String, completion: @escaping WeatherByCityUseCaseCompletion)
+    func fromLocationIfAvailable(completion: @escaping WeatherByCityUseCaseCompletion)
 }
 
 final class GetWeatherUseCaseImpl: GetWeatherUseCase {
@@ -29,6 +29,20 @@ final class GetWeatherUseCaseImpl: GetWeatherUseCase {
             switch result {
             case .success(let model):
                 completion(.success(model))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func fromLocationIfAvailable(completion: @escaping WeatherByCityUseCaseCompletion) {
+        getCityNameUseCase.run { [weak self] result in
+            guard let self = self else {
+                return
+            }
+            switch result {
+            case .success(let city):
+                run(city: city, completion: completion)
             case .failure(let error):
                 completion(.failure(error))
             }
