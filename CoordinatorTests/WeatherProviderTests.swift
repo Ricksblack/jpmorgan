2c6a2c73cfa53f6ee06853bda6860c03a3483e9d
Ricardo Moreno <rmm_92@outlook.com>
2023-04-16 12:32:28 -0700

    Created WeatherProviderTests

---
+++ CoordinatorTests/WeatherProviderTests.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherProviderTests.swift
+//  CoordinatorTests
+//
+//  Created by Ricardo Moreno on 4/16/23.
+//
+
+import XCTest
+import Coordinator
+
+final class WeatherProviderTests: XCTestCase {
+
+    
+}
