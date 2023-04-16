d44ea37e3b36dc94b3c45b8f93329711a06104d0
Ricardo Moreno <rmm_92@outlook.com>
2023-04-15 19:58:22 -0700

    Created GetUserLocationUseCaseImpl and UserLocationCoordinatesModel to be retrieved once system has checked permissions

---
+++ Coordinator/Domain/GetUserLocationUseCase.swift
@@ -0,0 +1,67 @@
+//
+//  GetUserLocationUseCase.swift
+//  Coordinator
+//
+//  Created by Ricardo Moreno on 4/15/23.
+//
+
+import Foundation
+import CoreLocation
+
+protocol GetUserLocationUseCase {
+    typealias UserLocationUseCaseCompletion = (Result<UserLocationCoordinatesModel, Error>) -> Void
+    func run(completion: @escaping UserLocationUseCaseCompletion)
+}
+
+final class GetUserLocationUseCaseImpl: NSObject, GetUserLocationUseCase {
+    private var locationManager: CLLocationManager
+    var completion: UserLocationUseCaseCompletion?
+
+    init(locationManager: CLLocationManager = CLLocationManager()) {
+        self.locationManager = locationManager
+    }
+    
+    func run(completion: @escaping UserLocationUseCaseCompletion) {
+        self.completion = completion
+        checkIfLocationServicesAreEnabled()
+    }
+}
+
+// MARK: - PRIVATE FUNCTIONS
+
+private extension GetUserLocationUseCaseImpl {
+    func checkIfLocationServicesAreEnabled() {
+        locationManager.delegate = self
+    }
+
+    func checkLocalizationAuthorization() {
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Maybe your parents are watching you!")
+            completion?(.failure(NSError()))
+        case .denied:
+            print("Why are you so bad :(")
+            completion?(.failure(NSError()))
+        case .authorizedAlways, .authorizedWhenInUse:
+            getUserLocation()
+        @unknown default:
+            print("Compiler was complaining so I added this")
+        }
+    }
+    
+    func getUserLocation() {
+        let model = UserLocationCoordinatesModel(latitute: locationManager.location?.coordinate.latitude.description ?? "",
+                                            longitude: locationManager.location?.coordinate.longitude.description ?? "")
+        completion?(.success(model))
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+
+extension GetUserLocationUseCaseImpl: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocalizationAuthorization()
+    }
+}
---
+++ Coordinator/Domain/Models/UserLocationCoordinatesModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserLocationCoordinatesModel.swift
+//  Coordinator
+//
+//  Created by Ricardo Moreno on 4/15/23.
+//
+
+import Foundation
+
+struct UserLocationCoordinatesModel {
+    let latitute: String
+    let longitude: String
+}
