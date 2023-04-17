ec684ea8f487616bb17d1de6edadc00fa40f667a
Ricardo Moreno <rmm_92@outlook.com>
2023-04-17 00:56:26 -0700

    Removed unnecessary code

--- Coordinator/Data/HTTPProtocol.swift
+++ Coordinator/Data/HTTPProtocol.swift
@@ -21,28 +21,9 @@ extension URLSession: HTTPProtocol {
                   response.statusCode == 200  {
                 completion(.success(data))
             } else {
-                // define error type
                 completion(.failure(NSError()))
             }
         }
         .resume()
     }
 }
-
-
-
-/*
-let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=6bd2b66fc1707a80a03c3b6ebd0c20b2"
-let url = URL(string: urlString)!
-URLSession.shared.dataTask(with: url) { data, response, error in
-    guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-        return
-    }
-    do {
-        let model = try JSONDecoder().decode(WeatherRoot.self, from: data)
-        print(model)
-    } catch {
-        print(error)
-    }
-}.resume()
-*/
